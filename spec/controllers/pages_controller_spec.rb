898f84a77be969f0676da8360b4b9cdc8fc09947
sly <ei.nselbst@GmaIl.com>
2011-03-16 22:33:39 +0100

    added title variable to pages controller

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -9,11 +9,33 @@ describe PagesController do
     end
   end
 
+  describe "GET 'about'" do
+    it "should be successful" do
+      get 'about'
+      response.should be_success
+    end
+  end
+
   describe "GET 'contact'" do
     it "should be successful" do
       get 'contact'
       response.should be_success
     end
+  end
+
+  it "should have the right title" do
+    get 'home'
+    response.should have_selector("title", :content => "JURAils | Home")
+  end
+
+  it "should have the right title" do
+    get 'about'
+    response.should have_selector("title", :content => "JURAils | About")
+  end
+
+  it "should have the right title" do
+    get 'contact'
+    response.should have_selector("title", :content => "JURAils | Contact")
   end
 
 end
