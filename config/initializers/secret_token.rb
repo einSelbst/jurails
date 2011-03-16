d24bbfded1ba3ec4cbe4bdc99de9b34b6e3d5474
sly <ei.nselbst@GmaIl.com>
2011-03-16 17:37:10 +0100

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jurails::Application.config.secret_token = 'ee0a03390be38fa40e9e2cdc5070765c5c256806f0097fee29e3ef5aadcf7269502e15badf2e585edd6264587e8c8fc354c5cc21d1a52ae76c631bef2ee8b370'
